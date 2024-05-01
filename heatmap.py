a741dda0da2a39910b412a84ed59efe86753d41f
Kristian Want <56544840+kristianwant@users.noreply.github.com>
2024-05-01 13:56:44 +0200

    Update heatmap.py
    Update

--- heatmap.py
+++ heatmap.py
@@ -31,15 +31,20 @@ tick_levels = np.arange(0, 3.6, 0.5)  # tick levels at 0.5 intervals
 # Plotting the contour map
 fig, ax = plt.subplots()
 contour_plot = plt.contourf(xi, yi, zi, levels=fine_levels, cmap=plt.cm.jet, extend='both')
-#plt.contour(xi, yi, zi, levels=[0.49], colors='black', linewidths=0.5)  # Adding contour line at z = 0.4
-color_bar = plt.colorbar(contour_plot, ticks=tick_levels)  # Show color scale with specified ticks
+contour_plot = plt.contour(xi, yi, zi, levels=[0.39], colors='black', linewidths=1, linestyles='dotted')  # Single contour line at z=0.34
+# Plotting the filled contour map
+filled_contour = plt.contourf(xi, yi, zi, levels=fine_levels, cmap=plt.cm.jet, extend='both')
+plt.contour(xi, yi, zi, levels=[0.35], colors='black', linewidths=1, linestyles='dotted')  # Single contour line at z=0.34
+
+# Generate the color bar based on the filled contour plot
+color_bar = plt.colorbar(filled_contour, ticks=tick_levels)  # Show color scale with specified ticks
 
 # Set the background color of the plot to match the highest color in the colormap
 ax.set_facecolor(plt.cm.jet(1.0))  # jet(1.0) retrieves the last color in the jet colormap
 
 # Setting the limits of the y-axis and x-axis
-ax.set_ylim(-18, -14)
-ax.set_xlim(5.5, 6.0)
+ax.set_ylim(-16, -7.5)
+ax.set_xlim(5, 10)
 
 plt.xlabel('Log(K$_A$) (µM)')
 plt.ylabel('ΔH (kcal/mol)')
